d98b9bb50381b247e6c982964336e33bb5b5c71f
Nagendra Kumar <nagendra.msft@hotmail.com>
2016-05-26 23:10:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php 
+   echo '<p>Hello World</p>'; 
+ if( $_GET["name"] || $_GET["email"] || $_GET["contact"])
+{
+echo "Welcome: ". $_GET['name']. "<br />";
+echo "Your Email is: ". $_GET["email"]. "<br />";
+echo "Your Mobile No. is: ". $_GET["contact"];
+}  
+ ?> 
  </body>
 </html
