2a4c8cbe59d9d309983be798eb0e76737d1ee105
Nagendra Kumar <nagendra.msft@hotmail.com>
2016-05-26 23:13:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,14 @@ echo "Welcome: ". $_GET['name']. "<br />";
 echo "Your Email is: ". $_GET["email"]. "<br />";
 echo "Your Mobile No. is: ". $_GET["contact"];
 }  
- ?> 
+ ?>
+ 
+ if( $_POST["name"] || $_POST["email"] || $_POST["contact"])
+{
+echo "Welcome: ". $_POST['name']. "<br />";
+echo "Your Email is: ". $_POST["email"]. "<br />";
+echo "Your Mobile No. is: ". $_POST["contact"];
+}
+?>
  </body>
 </html
